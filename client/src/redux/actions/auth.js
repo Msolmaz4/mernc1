7cd1e84d2ac55cfdd2d08200338ebaf04fc3d870
kyk <86296198+Msolmaz4@users.noreply.github.com>
2023-04-09 23:16:10 +0200

    actionstemel

---
+++ client/src/redux/actions/auth.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+export const registerAction =()=> async (dispatch)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const loginrAction =()=> async (dispatch)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
