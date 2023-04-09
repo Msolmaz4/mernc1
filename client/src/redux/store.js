f93663e4f694fc26a1ceab3fdd68d83159cfebb8
kyk <86296198+Msolmaz4@users.noreply.github.com>
2023-04-09 23:13:00 +0200

    store tanimlama

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,14 +1,16 @@
 import   { applyMiddleware, combineReducers, createStore }  from "redux"
 import thunk from "redux-thunk"
 import { composeWithDevTools } from 'redux-devtools-extension';
-
+import authReducer from "./reducers/auth";
 
 
 const initialState ={
 
 }
 
-const reducers = combineReducers({})
+const reducers = combineReducers({
+   auth: authReducer
+})
 
 
 const store = createStore(reducers,initialState,composeWithDevTools(applyMiddleware(thunk)))
