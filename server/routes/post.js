01eeed0ff00f7e4eca7489145c2df12314cd961d
kyk <86296198+Msolmaz4@users.noreply.github.com>
2023-04-17 23:20:57 +0200

    posts

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const {register,login} =require('../controllers/auth.js')
+
+const { getPosts, createPost, updatePost, deletePost } = require('../controllers/post.js')
 
 const router = express.Router()
 
 
 router.get('/getPosts',getPosts)
-router.post('/createPosts',createPosts)
+router.post('/createPosts',createPost)
 router.patch('/updatePost/:id',updatePost)
 router.delete('/deletePost/:id',deletePost)
 
